7f10aaf24c76c3afa1b7eced6047135b8f67e5c9
MiwMiw1 <77629887+MiwMiw1@users.noreply.github.com>
2022-02-06 01:09:05 +0100

    comments done (NO DOCSTRING)

--- scripts/NSIProjet3.js
+++ scripts/NSIProjet3.js
@@ -8,7 +8,7 @@ const inputIdKey = "qte";
 
 // === global variables  ===
 var total = 0; // total cost of selected products
-var msgStatement = false; // statement of exceeded budget message
+var exceededBuget = false; // statement of exceeded budget message
 
 // function called when page is loaded, it performs initializations 
 var init = function () {
@@ -16,7 +16,7 @@ var init = function () {
 }
 window.addEventListener("load", init);
 
-// OK
+// not our creation
 var createShop = function () {
 	var shop = document.getElementById("boutique");
 	for(var i = 0; i < catalog.length; i++) {
@@ -24,7 +24,7 @@ var createShop = function () {
 	};
 }
 
-// OK
+// not our creation
 var createProduct = function (product, index) {
 	// creating product's container
 	var block = document.createElement("div");
@@ -40,7 +40,7 @@ var createProduct = function (product, index) {
 	return block;
 }
 
-// OK
+// not our creation
 var createBlock = function (tag, content, cssClass) {
 	var element = document.createElement(tag);
 	if (cssClass != undefined) {
@@ -50,7 +50,137 @@ var createBlock = function (tag, content, cssClass) {
 	return element;
 }
 
-// OK
+// function done
+var createFigureBlock = function () {
+	// create a figure => (a bit overkilled function)
+	return createBlock("figure", "");
+}
+
+// function done
+var createImage = function (product) {
+	// create an img tag and return it filled with product image
+	var img = createBlock("img", "");
+	img.setAttribute("src", product.image);
+	return img;
+}
+
+// function done
+var search = function(research) {
+	const search = research.value.toLowerCase();
+	for (item in catalog) {
+		var product = catalog[item].name.toLowerCase();
+		// ternary => check if the value of search variable is in products title and display the produt or not
+		product.includes(search) ? document.getElementById(item + "-product").style.display = 'inline-block'
+		: document.getElementById(item + "-product").style.display = 'none';
+	};
+}
+
+// function done
+window.onload = function() {
+	// call search function everytime user's let his keys up
+	const research = document.getElementById("filter");
+	research.addEventListener("keyup", function() {
+		search(research);
+	});
+}
+
+// function done
+var cartStatement = function() {
+	const cart = document.getElementById("achats");
+	// check if budget is exceeded and display or not an exceeded budget message
+	if (total > 400 && exceededBuget == false) {
+		var errorMsg = cart.appendChild(createBlock("div", "Budget dépassé"));
+		errorMsg.className = "Budget dépassé";
+		errorMsg.id = "exceeded-budget";
+		exceededBuget = true;
+	} else if (total <= 400 && exceededBuget == true) {
+		document.getElementById("exceeded-budget").remove();
+		exceededBuget = false;
+	};	
+}
+
+// functione done
+var cartValue = function(removeStatus=false, price, quantity) {
+	price = parseInt(price);
+	quantity = parseInt(quantity);
+	// ternary => check if the order value has to be removed or added to the cart's value
+	// then update cart's value
+	removeStatus == true ? total = total - quantity*price : total = total + quantity*price;
+	document.getElementById("montant").innerHTML = total;
+	cartStatement(); // check if has to display an exceeded budget message
+}
+
+// function done
+var sendProduct = function(divCart, id, cartId,  product, description, quantity, price) {
+	// creating the ordered product div
+	var sendToCart = divCart.appendChild(createBlock("div", ""));
+	sendToCart.className = "achat";
+	sendToCart.id = id.replace("order", "cart");
+	sendToCart.appendChild(createFigureBlock()).appendChild(createImage(product));
+	sendToCart.appendChild(createBlock("h4", description));
+	
+	// creating the ordered product quantity div
+	var divQuantity = sendToCart.appendChild(createBlock("div", quantity));
+	divQuantity.className = "quantite";
+	divQuantity.id = cartId.replace("cart", "quantity");
+
+	// creating the ordered product price div
+	var divPrice = sendToCart.appendChild(createBlock("div", price));
+	divPrice.className = "prix";
+
+	// creating the button to remove the ordered product from the cart
+	var removeButton = document.createElement("button");
+	removeButton.className = "retirer";
+	removeButton.id = id.replace("order", "remove");
+	removeButton.onclick = function() {
+		removeQuantity = document.getElementById(divQuantity.id).innerHTML;
+		document.getElementById(sendToCart.id).remove();
+		cartValue(removeStatus=true, price, removeQuantity); // updating the cart's value
+	};
+	
+	var divRemove = sendToCart.appendChild(createBlock("div", ""));
+	divRemove.className = "controle";
+	divRemove.appendChild(removeButton);
+
+	cartValue(removeStatus=false, price, quantity);
+}
+
+// function done
+var updateQuantity = function(cartId, price, quantity) {
+	var freshQuantity = document.getElementById(cartId).getElementsByClassName("quantite")[0];
+
+	// check if the selected product is already ordered more than 9 times
+	if ((parseInt(freshQuantity.innerHTML) + parseInt(quantity)) > 9) {
+		alert("Vous ne pouvez commander que 9 fois le même produit.");
+	} else {
+		// update the product quantity and the cart's value
+		freshQuantity.innerHTML = (parseInt(freshQuantity.innerHTML) + parseInt(quantity)).toString();
+		cartValue(removeStatus=false, price, quantity);
+	};
+}
+
+// function done
+var addToCart = function(id) {
+	// collect needed data to add a product to the cart
+	const divCart = document.getElementById("achats");
+	var product = catalog[parseInt(id.split("-")[0])];
+	var orderId = id.replace("order", "qte");
+	var quantity = document.getElementById(orderId).value;	
+	var description = product.description;
+	var price = product.price;
+	var cartId = id.replace("order", "cart");
+
+	// check if budget is exceeded 
+	if (exceededBuget == true) {
+		alert("Le budget est dépassé.");
+	} else { 
+		// ternary => check if selected product is already in cart or not and act accordingly
+		document.getElementById(cartId) == null ? sendProduct(divCart, id, cartId, product, description, quantity, price)
+		: updateQuantity(cartId, price, quantity);
+	};
+}
+
+// function done
 var createOrderControlBlock = function (index) {
 	var control = document.createElement("div");
 	control.className = "controle";
@@ -74,13 +204,14 @@ var createOrderControlBlock = function (index) {
 
 	// setting input's conditions
 	input.addEventListener("input", function() {
+		// ternary => set up input's limits and button style according to input's value
 		input.value > 9 ? input.value = 9 : input.value;
 		input.value < 0 ? input.value = 0 : input.value;
 		input.value > 0 ? orderButton.style.opacity = 1 : orderButton.style.opacity = 0.25;
 	});
 
 	orderButton.onmousedown = function() {
-		if (input.value != 0) {
+		if (input.value > 0) {
 			orderButton.style.opacity = 0.25;
 			addToCart(this.id);
 			document.getElementById(index + "-" + inputIdKey).value = 0; // setting back input's value to 0
@@ -92,118 +223,4 @@ var createOrderControlBlock = function (index) {
 	
 	// the built control div node is returned
 	return control;
-}
-
-// OK
-var createFigureBlock = function () {
-	return createBlock("figure", "");
-}
-
-// OK
-var createImage = function (product) {
-	var img = createBlock("img", "");
-	img.setAttribute("src", product.image);
-	return img;
-}
-
-// OK
-var search = function(research) {
-	const search = research.value.toLowerCase();
-	for (item in catalog) {
-		var product = catalog[item].name.toLowerCase();
-		product.includes(search) ? document.getElementById(item + "-product").style.display = 'inline-block'
-		: document.getElementById(item + "-product").style.display = 'none';
-	};
-}
-
-// OK
-window.onload = function() {
-	const research = document.getElementById("filter");
-	research.addEventListener("keyup", function() {
-		search(research);
-	});
-}
-
-// OK
-var cartStatement = function() {
-	const cart = document.getElementById("achats");
-	if (total > 400 && msgStatement == false) {
-		var errorMsg = cart.appendChild(createBlock("div", "Budget dépassé"));
-		errorMsg.className = "Budget dépassé";
-		errorMsg.id = "exceeded-budget";
-		msgStatement = true;
-	} else if (total <= 400 && msgStatement == true) {
-		document.getElementById("exceeded-budget").remove();
-		msgStatement = false;
-	};	
-}
-
-// OK
-var cartValue = function(removeStatus=false, price, quantity) {
-	price = parseInt(price);
-	quantity = parseInt(quantity);
-	removeStatus == true ? total = total - quantity*price : total = total + quantity*price;
-	var divTotal = document.getElementById("montant");
-	divTotal.innerHTML = total;
-	cartStatement();
-}
-
-// OK
-var sendProduct = function(divCart, id, cartId,  product, description, quantity, price) {
-	var sendToCart = divCart.appendChild(createBlock("div", ""));
-	sendToCart.className = "achat";
-	sendToCart.id = id.replace("order", "cart");
-	sendToCart.appendChild(createFigureBlock()).appendChild(createImage(product));
-	sendToCart.appendChild(createBlock("h4", description));
-	
-	var divQuantity = sendToCart.appendChild(createBlock("div", quantity));
-	divQuantity.className = "quantite";
-	divQuantity.id = cartId.replace("cart", "quantity");
-
-	var divPrice = sendToCart.appendChild(createBlock("div", price));
-	divPrice.className = "prix";
-
-	var removeButton = document.createElement("button");
-	removeButton.className = "retirer";
-	removeButton.id = id.replace("order", "remove");
-	removeButton.onclick = function() {
-		removeQuantity = document.getElementById(divQuantity.id).innerHTML;
-		document.getElementById(sendToCart.id).remove();
-		cartValue(removeStatus=true, price, removeQuantity);
-	};
-	
-	var divRemove = sendToCart.appendChild(createBlock("div", ""));
-	divRemove.className = "controle";
-	divRemove.appendChild(removeButton);
-
-	cartValue(removeStatus=false, price, quantity);
-}
-
-// OK
-var updateQuantity = function(cartId, price, quantity) {
-	var product = document.getElementById(cartId);
-	var freshQuantity = product.getElementsByClassName("quantite")[0];
-	if ((parseInt(freshQuantity.innerHTML) + parseInt(quantity)) > 9) {
-		alert("Vous ne pouvez commander que 9 fois le même produit.");
-	} else {
-		freshQuantity.innerHTML = (parseInt(freshQuantity.innerHTML) + parseInt(quantity)).toString();
-		cartValue(removeStatus=false, price, quantity);
-	};
-}
-
-// OK
-var addToCart = function(id) {
-	const divCart = document.getElementById("achats");
-	var product = catalog[parseInt(id.split("-")[0])];
-	var orderId = id.replace("order", "qte");
-	var quantity = document.getElementById(orderId).value;	
-	var description = product.description;
-	var price = product.price;
-	var cartId = id.replace("order", "cart");
-	if (total > 400) {
-		alert("Le budget est dépassé.");
-	} else {
-		document.getElementById(cartId) == null ? sendProduct(divCart, id, cartId, product, description, quantity, price)
-		: updateQuantity(cartId, price, quantity);
-	};
 }
